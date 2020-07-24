273b392311e2d27bc9154327299c191d371acbae
Danil Kozlov <megacokes@gmail.com>
2020-07-24 21:25:29 +0500

    Create form carcass

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="wrapper">
+      <header>
+        header
+      </header>
+      <main>
+        main
+      </main>
+      <aside>
+        aside
+      </aside>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
 
 <style lang="sass">
+@import "./assets/styles/variables.sass";
+
+*
+  padding: 0
+  margin: 0
+
+.flex
+  display: flex
+
+body
+  color: #2c3e50
+  background-color: #AFDAE8
+
 #app
   font-family: Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
-  margin-top: 60px
+  display: flex
+  justify-content: center
+
+.wrapper
+  display: flex
+  flex-flow: row wrap
+  width: 1400px
+  padding: $margin*2
+
+  background-color: white
+
+header
+  height: 80px
+  flex: 1 100%
+
+  background-color: #81B3FF
+
+main
+  order: 1
+  flex: 3 0px
+  background-color: green
+
+aside
+  order: 2
+  flex: 1 auto
+  background-color: yellow
 </style>
